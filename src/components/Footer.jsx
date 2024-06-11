1d87e700a908742b58fae93a5487c7b2bfd82bc4
vinayka2601 <147977944+vinayka2601@users.noreply.github.com>
2024-06-11 19:59:38 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+    <div>
+      <h1>Naruto</h1>
+      <p>@all right reserved</p>
+    </div>
+  <div>
+    <h5>Follow us</h5>
+    <div>
+      <a href="www.linkedin.com/in/vinayka-awasthi-078a88298">LinkedIn</a>
+      <a href="https://github.com/vinayka2601">GitHub</a>
+    </div>
+  </div>
+  </footer>
+}
+
+export default Footer
